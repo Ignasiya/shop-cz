500b60a50e332bc7cb92372f282914c1dd00ec7c
Ignasiya <ignasiya93@gmail.com>
2025-03-24 07:27:06 +0300

    Правки

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,14 +1,8 @@
-import { categoryStore } from '@/entities/category'
 import { CategoriesList } from '@/features/categories-list.tsx'
 import { ProductList } from '@/features/product-list'
 import Head from 'next/head'
-import { useEffect } from 'react'
 
 export default function Products() {
-  useEffect(() => {
-    categoryStore.fetchCategories()
-  })
-
   return (
     <div className='flex flex-col gap-5'>
       <Head>
